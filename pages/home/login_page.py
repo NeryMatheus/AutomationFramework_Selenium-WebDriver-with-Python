f575d3efc795209291aafcbae6c35eb0eae25d63
Matheus Nery <matheusnery345@outlook.com>
2021-07-29 12:07:23 -0400

    Foi atualizado o arquivo python "login_page" do pacote pages

--- pages/home/login_page.py
+++ pages/home/login_page.py
@@ -14,18 +14,6 @@ class LoginPage(SeleniumDriver):
     _passwordField = "password"
     _loginButton = "//input[@value='Login']"
 
-    def getLoginLink(self):
-        return self.driver.find_element(By.XPATH, self._loginLink)
-
-    def getEmailField(self):
-        return self.driver.find_element(By.ID, self._emailField)
-
-    def getPasswordField(self):
-        return self.driver.find_element(By.ID, self._passwordField)
-
-    def getLoginButton(self):
-        return self.driver.find_element(By.XPATH, self._loginButton)
-
     # ================================================================================
 
     def clickLoginLink(self):
